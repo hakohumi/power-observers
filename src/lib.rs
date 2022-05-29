680e64ca2d23d295d3149a123b3589fdfd364ce2
filu <mihumihu32@gmail.com>
2022-05-29 22:15:33 +0900

    Move processing to be received from ADC

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use anyhow::{Ok, Result};
 
-use embedded_hal::adc::OneShot;
-use esp_idf_hal::adc;
-use esp_idf_hal::prelude::Peripherals;
 use std::sync::{Condvar, Mutex};
 use std::thread;
 use std::{sync::Arc, time::*};
@@ -21,21 +18,12 @@ pub fn run() -> Result<()> {
         thread::sleep(Duration::from_secs(1));
     }
 
-    let peripherals = Peripherals::take().unwrap();
-    let pins = peripherals.pins;
-    let mut a2 = pins.gpio34.into_analog_atten_11db()?;
-    let mut powered_adc1 = adc::PoweredAdc::new(
-        peripherals.adc1,
-        adc::config::Config::new().calibration(true),
-    )?;
-
     let (eventloop, _subscription) = test_eventloop()?;
     let _timer = init_timer(eventloop)?;
 
     let mutex = Arc::new((Mutex::new(None), Condvar::new()));
 
     let mut wait = mutex.0.lock().unwrap();
-    let mut hall_sensor = peripherals.hall_sensor;
 
     #[allow(unused)]
     let cycles: u32 = loop {
@@ -48,16 +36,6 @@ pub fn run() -> Result<()> {
                 .unwrap()
                 .0;
 
-            println!(
-                "Hall sensor reading: {}mV",
-                powered_adc1.read(&mut hall_sensor).unwrap()
-            );
-            println!(
-                "A2 sensor reading: {}mV",
-                powered_adc1.read(&mut a2).unwrap()
-            );
-
-            // TODO: ここでサーバに接続して、電力センサーからの値を送信する。
         }
     };
     Ok(())
--- src/timer.rs
+++ src/timer.rs
@@ -7,10 +7,23 @@ use esp_idf_svc::systime::EspSystemTime;
 use esp_idf_svc::timer::{EspTimer, EspTimerService};
 use std::time::Duration;
 
+use embedded_hal::adc::OneShot;
+use esp_idf_hal::adc;
+use esp_idf_hal::prelude::Peripherals;
+
 use crate::eventloop::EventLoopMessage;
 
 pub fn init_timer(mut eventloop: EspBackgroundEventLoop) -> Result<EspTimer> {
     use embedded_svc::event_bus::Postbox;
+
+    let peripherals = Peripherals::take().unwrap();
+    let pins = peripherals.pins;
+
+    let mut a2 = pins.gpio34.into_analog_atten_11db()?;
+    let mut powered_adc1 = adc::PoweredAdc::new(
+        peripherals.adc1,
+        adc::config::Config::new().calibration(true),
+    )?;
 
     let mut periodic_timer = EspTimerService::new()?.timer(move || {
         println!("Tick from periodic timer");
@@ -19,10 +32,17 @@ pub fn init_timer(mut eventloop: EspBackgroundEventLoop) -> Result<EspTimer> {
 
         eventloop.post(&EventLoopMessage::new(now), None).unwrap();
 
-        println!("eventloop test");
+        println!("eventloop test {:?}", now.to_owned());
+
+        // TODO: ここでサーバに接続して、電力センサーからの値を送信する。
+
+        println!(
+            "A2 sensor reading: {}mV",
+            powered_adc1.read(&mut a2).unwrap()
+        );
     })?;
 
-    periodic_timer.every(Duration::from_secs(5))?;
+    periodic_timer.every(Duration::from_millis(8))?;
 
     Ok(periodic_timer)
 }
