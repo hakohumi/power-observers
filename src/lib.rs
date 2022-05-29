a0e22812b472514742d82f07ffd0f0ac47239e08
filu <mihumihu32@gmail.com>
2022-05-29 20:50:11 +0900

    Split timer

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,16 @@
-use embedded_svc::sys_time::SystemTime;
-use embedded_svc::timer::{OnceTimer, PeriodicTimer, TimerService};
-
-use esp_idf_svc::eventloop::EspBackgroundEventLoop;
-use esp_idf_svc::systime::EspSystemTime;
-use esp_idf_svc::timer::{EspTimer, EspTimerService};
-
 use anyhow::{Ok, Result};
+
 use embedded_hal::adc::OneShot;
 use esp_idf_hal::adc;
 use esp_idf_hal::prelude::Peripherals;
 use std::sync::{Condvar, Mutex};
-use std::thread;
 use std::{sync::Arc, time::*};
 
 mod eventloop;
-use eventloop::test_eventloop;
-use eventloop::EventLoopMessage;
+use crate::eventloop::test_eventloop;
+
+mod timer;
+use timer::test_timer;
 
 pub fn run() -> Result<()> {
     println!("Hello, world!");
@@ -60,32 +55,4 @@ pub fn run() -> Result<()> {
         }
     };
     Ok(())
-}
-
-fn test_timer(mut eventloop: EspBackgroundEventLoop) -> Result<EspTimer> {
-    use embedded_svc::event_bus::Postbox;
-
-    println!("About to schedule a one-shot timer for after 2 seconds");
-    let mut once_timer = EspTimerService::new()?.timer(|| {
-        println!("One-shot timer triggered");
-    })?;
-
-    once_timer.after(Duration::from_secs(2))?;
-
-    thread::sleep(Duration::from_secs(3));
-
-    println!("About to schedule a periodic timer every five seconds");
-    let mut periodic_timer = EspTimerService::new()?.timer(move || {
-        println!("Tick from periodic timer");
-
-        let now = EspSystemTime {}.now();
-
-        eventloop.post(&EventLoopMessage::new(now), None).unwrap();
-
-        println!("eventloop test");
-    })?;
-
-    periodic_timer.every(Duration::from_secs(5))?;
-
-    Ok(periodic_timer)
 }
---
+++ src/timer.rs
@@ -0,0 +1,39 @@
+use embedded_svc::sys_time::SystemTime;
+use embedded_svc::timer::{OnceTimer, PeriodicTimer, TimerService};
+
+use anyhow::{Ok, Result};
+use esp_idf_svc::eventloop::EspBackgroundEventLoop;
+use esp_idf_svc::systime::EspSystemTime;
+use esp_idf_svc::timer::{EspTimer, EspTimerService};
+use std::thread;
+use std::time::Duration;
+
+use crate::eventloop::EventLoopMessage;
+
+pub fn test_timer(mut eventloop: EspBackgroundEventLoop) -> Result<EspTimer> {
+    use embedded_svc::event_bus::Postbox;
+
+    println!("About to schedule a one-shot timer for after 2 seconds");
+    let mut once_timer = EspTimerService::new()?.timer(|| {
+        println!("One-shot timer triggered");
+    })?;
+
+    once_timer.after(Duration::from_secs(2))?;
+
+    thread::sleep(Duration::from_secs(3));
+
+    println!("About to schedule a periodic timer every five seconds");
+    let mut periodic_timer = EspTimerService::new()?.timer(move || {
+        println!("Tick from periodic timer");
+
+        let now = EspSystemTime {}.now();
+
+        eventloop.post(&EventLoopMessage::new(now), None).unwrap();
+
+        println!("eventloop test");
+    })?;
+
+    periodic_timer.every(Duration::from_secs(5))?;
+
+    Ok(periodic_timer)
+}
