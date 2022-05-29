ada0e36409ab3eb59285dd060b7f7d90418628f8
filu <mihumihu32@gmail.com>
2022-05-29 21:10:10 +0900

    Delete unnecessary codes

--- src/timer.rs
+++ src/timer.rs
@@ -1,11 +1,10 @@
 use embedded_svc::sys_time::SystemTime;
-use embedded_svc::timer::{OnceTimer, PeriodicTimer, TimerService};
+use embedded_svc::timer::{PeriodicTimer, TimerService};
 
 use anyhow::{Ok, Result};
 use esp_idf_svc::eventloop::EspBackgroundEventLoop;
 use esp_idf_svc::systime::EspSystemTime;
 use esp_idf_svc::timer::{EspTimer, EspTimerService};
-use std::thread;
 use std::time::Duration;
 
 use crate::eventloop::EventLoopMessage;
@@ -13,16 +12,6 @@ use crate::eventloop::EventLoopMessage;
 pub fn init_timer(mut eventloop: EspBackgroundEventLoop) -> Result<EspTimer> {
     use embedded_svc::event_bus::Postbox;
 
-    println!("About to schedule a one-shot timer for after 2 seconds");
-    let mut once_timer = EspTimerService::new()?.timer(|| {
-        println!("One-shot timer triggered");
-    })?;
-
-    once_timer.after(Duration::from_secs(2))?;
-
-    thread::sleep(Duration::from_secs(3));
-
-    println!("About to schedule a periodic timer every five seconds");
     let mut periodic_timer = EspTimerService::new()?.timer(move || {
         println!("Tick from periodic timer");
 
